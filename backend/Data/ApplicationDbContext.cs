d70d33d46ddb341c9af970f55e110e8b79ffd478
ThaminduNethmina <himanshavithanage@gmail.com>
2025-03-03 16:44:51 +0530

    Configure entity framework and create migration

---
+++ backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        DbSet<Stock> Stocks { get; set; }
+        DbSet<Comment> Comments { get; set; }   
+    }
+}
