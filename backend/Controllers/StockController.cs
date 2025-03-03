bdd601c457fd94b21f8a3f4773c28c90d257894c
ThaminduNethmina <himanshavithanage@gmail.com>
2025-03-04 00:10:43 +0530

    Refactor Stock controller with repository pattern

--- backend/Controllers/StockController.cs
+++ backend/Controllers/StockController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using backend.Data;
 using backend.DTOs.Stock;
+using backend.Interfaces;
 using backend.Mappers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,24 +15,24 @@ namespace backend.Controllers
     [Route("api/stock")]
     public class StockController : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
-        public StockController(ApplicationDbContext context)
+        private readonly IStockRepository _stockRepository;
+        public StockController(IStockRepository stockRepository)
         {
-            _context = context;
+            _stockRepository = stockRepository;
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            var stocks = await _context.Stocks.ToListAsync();
+            var stocks = await _stockRepository.GetAllAsync();
             var stockDTO = stocks.Select(s => s.ToStockDTO());
-            return Ok(stocks);
+            return Ok(stockDTO);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById([FromRoute] int id)
         {
-            var stock = await _context.Stocks.FindAsync(id);
+            var stock = await _stockRepository.GetByIdAsync(id);
             if (stock == null)
             {
                 return NotFound();
@@ -46,28 +47,20 @@ namespace backend.Controllers
         public async Task<IActionResult> Create([FromBody] CreateStockRequestDTO stockDTO)
         {
             var stock = stockDTO.ToStockFromCreateDTO();
-            await _context.Stocks.AddAsync(stock);
-            await _context.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetById), new { id = stock.Id }, stock.ToStockDTO());
+            var newStock = await _stockRepository.CreateAsync(stock);
+            return CreatedAtAction(nameof(GetById), new { id = newStock.Id }, stock.ToStockDTO());
         }
 
         [HttpPut("{id}")]
         public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDTO updateDTO)
         {
-            var stock = await _context.Stocks.FindAsync(id);
+            var stock = await _stockRepository.UpdateAsync(id, updateDTO);
             if (stock == null)
             {
                 return NotFound();
             }
             else
             {
-                stock.Symbol = updateDTO.Symbol;
-                stock.CompanyName = updateDTO.CompanyName;
-                stock.Purchase = updateDTO.Purchase;
-                stock.LastDiv = updateDTO.LastDiv;
-                stock.Industry = updateDTO.Industry;
-                stock.MarketCap = updateDTO.MarketCap;
-                await _context.SaveChangesAsync();
                 return Ok(stock.ToStockDTO());
             }
         }
@@ -75,15 +68,13 @@ namespace backend.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete([FromRoute] int id)
         {
-            var stock = await _context.Stocks.FindAsync(id);
+            var stock = await _stockRepository.DeleteAsync(id);
             if (stock == null)
             {
                 return NotFound();
             }
             else
             {
-                _context.Stocks.Remove(stock);
-                await _context.SaveChangesAsync();
                 return NoContent();
             }
         }
---
+++ backend/Repositories/StockRepository.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Data;
+using backend.DTOs.Stock;
+using backend.Interfaces;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Repositories
+{
+    public class StockRepository : IStockRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public StockRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Stock> CreateAsync(Stock stock)
+        {
+            await _context.Stocks.AddAsync(stock);
+            await _context.SaveChangesAsync();
+            return stock;
+        }
+
+        public async Task<Stock?> DeleteAsync(int id)
+        {
+            var stock = await _context.Stocks.FindAsync(id);
+            if (stock == null)
+            {
+                return null;
+            }
+            _context.Stocks.Remove(stock);
+            await _context.SaveChangesAsync();
+            return stock;
+        }
+
+        public async Task<List<Stock>> GetAllAsync()
+        {
+            return await _context.Stocks.ToListAsync();
+        }
+
+        public async Task<Stock?> GetByIdAsync(int id)
+        {
+            return await _context.Stocks.FindAsync(id);
+        }
+
+        public async Task<Stock?> UpdateAsync(int id, UpdateStockRequestDTO updateDTO)
+        {
+            var stock = await _context.Stocks.FindAsync(id);
+            if (stock == null)
+            {
+                return null;
+            }
+            stock.Symbol = updateDTO.Symbol;
+            stock.CompanyName = updateDTO.CompanyName;
+            stock.Purchase = updateDTO.Purchase;
+            stock.LastDiv = updateDTO.LastDiv;
+            stock.Industry = updateDTO.Industry;
+            stock.MarketCap = updateDTO.MarketCap;
+            await _context.SaveChangesAsync();
+            return stock;
+        }
+    }
+}
